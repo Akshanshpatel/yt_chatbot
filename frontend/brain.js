3f87168004a879ad780b85c5ec4297aedc7aefba
Akshansh Patel <akshanshpatel003@gmail.com>
2026-05-31 13:03:06 +0530

    url change

--- frontend/brain.js
+++ frontend/brain.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = "https://yt-chatbot-68zq.onrender.com/";
 
 const loadBtn = document.getElementById("loadBtn");
 const askBtn = document.getElementById("askBtn");
